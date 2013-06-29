e7973aa02e5b7037bd58b1b0604924e29fa70b28
Takenori <tknrkb@gmail.com>
2013-06-29 18:19:37 +0200

    upto simspark-0.2.4 and rc3d-0.6.7, but dose not work yet.

--- rcssserver3d.rb
+++ rcssserver3d.rb
@@ -5,10 +5,16 @@ require 'formula'
 
 class Rcssserver3d < Formula
   homepage ''
-  url 'http://downloads.sourceforge.net/project/simspark/rcssserver3d/0.6.6/rcssserver3d-0.6.6.tar.gz'
-  md5 '4d560e409700a869a8477b705cbb14c1'
+  url 'http://downloads.sourceforge.net/project/simspark/rcssserver3d/0.6.7/rcssserver3d-0.6.7.tar.gz'
+  sha1 '5ce75d95ebdc19aaf6ecfd5f7d8cb9e48435de03'
 
+  depends_on 'cmake' => :build
+  depends_on 'boost'
   depends_on 'simspark'
+
+  def patches
+    DATA
+  end
 
   def install
     # ENV.x11 # if your formula requires any X11 headers
@@ -16,8 +22,8 @@ class Rcssserver3d < Formula
 
     #system "./configure", "--disable-debug", "--disable-dependency-tracking",
     #                      "--prefix=#{prefix}"
-    system "cmake . #{std_cmake_parameters} -DUSE_COREFOUNDATION=OFF"
-    system "make install" # if this fails, try separate make/make install steps
+    system "cmake . #{std_cmake_parameters} -DUSE_COREFOUNDATION=OFF -DPDFLATEX_COMPILER=OFF"
+    system "make VERBOSE=1 install" # if this fails, try separate make/make install steps
   end
 
   def test
@@ -27,3 +33,43 @@ class Rcssserver3d < Formula
     system "false"
   end
 end
+
+__END__
+*** a/plugin/soccer/CMakeLists.txt	2013-06-29 17:54:16.000000000 +0200
+--- b/plugin/soccer/CMakeLists.txt	2013-06-29 17:55:14.000000000 +0200
+***************
+*** 136,142 ****
+  
+  add_library(soccer MODULE ${soccer_LIB_SRCS} ${soccer_LIB_HDRS})
+  
+! target_link_libraries(soccer ${spark_libs} ${OPENGL_gl_LIBRARY})
+  
+  if (NOT APPLE)
+     set_target_properties(soccer PROPERTIES VERSION 1.1.0 SOVERSION 1)
+--- 136,142 ----
+  
+  add_library(soccer MODULE ${soccer_LIB_SRCS} ${soccer_LIB_HDRS})
+  
+! target_link_libraries(soccer ${spark_libs} ${OPENGL_gl_LIBRARY} ${Boost_LIBRARIES})
+  
+  if (NOT APPLE)
+     set_target_properties(soccer PROPERTIES VERSION 1.1.0 SOVERSION 1)
+*** a/plugin/soccermonitor/CMakeLists.txt	2013-06-29 17:56:54.000000000 +0200
+--- b/plugin/soccermonitor/CMakeLists.txt	2013-06-29 17:57:31.000000000 +0200
+***************
+*** 24,30 ****
+  add_library(soccermonitor MODULE ${soccermonitor_LIB_SRCS}
+     ${soccermonitor_LIB_HDRS})
+  
+! target_link_libraries(soccermonitor ${spark_libs} ${OPENGL_LIBRARIES})
+  
+  if (NOT APPLE)
+     set_target_properties(soccermonitor PROPERTIES VERSION 1.0.0 SOVERSION 1)
+--- 24,30 ----
+  add_library(soccermonitor MODULE ${soccermonitor_LIB_SRCS}
+     ${soccermonitor_LIB_HDRS})
+  
+! target_link_libraries(soccermonitor ${spark_libs} ${OPENGL_LIBRARIES} ${Boost_LIBRARIES})
+  
+  if (NOT APPLE)
+     set_target_properties(soccermonitor PROPERTIES VERSION 1.0.0 SOVERSION 1)
--- simspark.rb
+++ simspark.rb
@@ -1,41 +1,22 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Simspark < Formula
   homepage ''
-  url 'http://downloads.sourceforge.net/project/simspark/simspark/0.2.3/simspark-0.2.3.tar.gz'
-  md5 'b653af47b014b0af99d5dda1e3949d7b'
+  url 'http://downloads.sourceforge.net/project/simspark/simspark/0.2.4/simspark-0.2.4.tar.gz'
+  sha1 'f1cee938564c5d8de01a38bab8340d78585ccb89'
 
   depends_on 'cmake' => :build
-  #find_package(Ruby REQUIRED)
-  #find_package(Freetype REQUIRED)
-  #find_package(DevIL REQUIRED)
-  depends_on 'devil'
-  #find_package(Boost REQUIRED thread COMPONENTS regex date_time)
   depends_on 'boost'
-  #find_package(wxWidgets COMPONENTS gl base core)
-  #depends_on 'wxmac'
-  #find_package(ODE REQUIRED)
-  depends_on 'ode'
-  #find_package(OpenGL REQUIRED)
-  #find_package(SDL REQUIRED)
   depends_on 'sdl'
-  #find_package(FMOD)
-  #find_package(ZLIB)
+  depends_on 'devil'
+  depends_on 'ode'
 
   def patches
     DATA
   end
 
   def install
-    # ENV.x11 # if your formula requires any X11 headers
-    # ENV.j1  # if your formula's build system can't parallelize
-
-    #system "./configure", "--disable-debug", "--disable-dependency-tracking",
-    #                      "--prefix=#{prefix}"
-    system "cmake . #{std_cmake_parameters} -DUSE_COREFOUNDATION=OFF"
+    system "cmake . #{std_cmake_parameters} -DUSE_COREFOUNDATION=OFF -DPDFLATEX_COMPILER=OFF" # don't make manual.tex
     system "make install" # if this fails, try separate make/make install steps
   end
 
